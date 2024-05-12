6a45e21428f546f968ed01893e284008e7218901
Rúbem Vieira <rubemvn17@gmail.com>
2024-05-12 10:56:02 -0300

    Config Inicial de arquivos

---
+++ felinos/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Content from "../Content/Content";
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
+
+const Layout = () => {
+	return (
+		<div>
+			<NavBar />
+			<Content />
+			<Footer />
+		</div>
+	);
+};
+
+export default Layout;
