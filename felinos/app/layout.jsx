52a83ad1b9dc2a6008b43624b381cb3e017739a0
Rúbem Vieira <rubemvn17@gmail.com>
2024-05-20 09:57:58 -0300

    Teste de fontes finalizado

--- felinos/app/layout.jsx
+++ felinos/app/layout.jsx
@@ -1,7 +1,6 @@
 // import { Montserrat } from "next/font/google";
 import StyledComponentsRegistry from "./registry";
 import GlobalStyles from "@/styles/GlobalStyles";
-import Head from "next/head";
 
 // const montserrat = Montserrat({
 // 	weight: ["400", "500", "600", "700", "800", "900"],
@@ -23,21 +22,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='pt-br'>
 			<GlobalStyles />
-			<Head>
-				<link
-					rel='preconnect'
-					href='https://fonts.googleapis.com'
-				/>
-				<link
-					rel='preconnect'
-					href='https://fonts.gstatic.com'
-					crossorigin
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'
-					rel='stylesheet'
-				/>
-			</Head>
+
 			<body
 			// className={montserrat.className}
 			>
