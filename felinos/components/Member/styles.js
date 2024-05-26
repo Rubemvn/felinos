a239d2b160e245a8565925c7803aae5cf76dfec5
Rúbem Vieira <rubemvn17@gmail.com>
2024-05-26 19:07:04 -0300

    test: one more test of height plan

--- felinos/components/Member/styles.js
+++ felinos/components/Member/styles.js
@@ -90,7 +90,7 @@ justify-content: space-between;
 align-items: center;
 border-radius: 1rem;
 overflow: hidden;
-min-height: 65.5rem;
+min-height: 67rem;
 flex-direction: column;
 background-color: ${(props) => (props.setPlan === "gold") ?
     props.theme.colors.gold01 :
